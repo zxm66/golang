18b8568a71e92e90f4c2663ba214195dd8c47bc4
zxm66 <1396729865@qq.com>
2020-07-16 08:47:14 +0800

    hello world

---
+++ test1/helloworld.go
@@ -0,0 +1,72 @@
+// Package main provides ...
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+		var_test1();
+		con_test1();
+
+
+		sum,sub:=calc(1,2);
+		fmt.Println(sum,sub);
+
+		go fmt.Println(add(1,2));
+		time.Sleep(time.Second * 1);
+		fmt.Println("hello world")
+}
+func add(a int , b int) int {
+		return a + b
+}
+func calc(a int ,b int) (int , int ) {
+		return a + b ,a - b
+}
+//标识符
+//关键字
+// 变量
+// 常量
+func  var_test1() {
+		var a int
+		var b string
+		var c bool
+		var d int = 8
+		var e string = "hello world"
+
+		fmt.Println(a,b,c,d,e)
+	
+}
+
+// 权限控制
+func Test1(){
+		fmt.Println("this is Test1");
+}
+
+func  con_test1(){
+		const a int = 9;
+		const b string = "hello world";
+		const c bool = false;
+		const d = 9;
+		fmt.Println(a,b,c,d);
+
+		const (
+				e string = "hello c"
+				f int = 0;
+				g 
+		)
+		fmt.Println(e,f,g);
+
+		const (
+				h =  1<<iota
+				j
+				k
+
+		)
+
+		fmt.Println(h,j,k);
+
+
+}
+
+
