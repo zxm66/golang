933751a8a139d9b98f36e0bff0724f51f462823d
zxm66 <1396729865@qq.com>
2020-07-19 02:48:07 +0800

    gc

--- test1/helloworld.go
+++ test1/helloworld.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
+		fmt.Println(helloworld());
+		fmt.Println(sayHelloWorld());
 		var_test1();
 		con_test1();
-
-
 		sum,sub:=calc(1,2);
 		fmt.Println(sum,sub);
 
@@ -17,11 +17,21 @@ func main() {
 		time.Sleep(time.Second * 1);
 		fmt.Println("hello world")
 }
+
+func helloworld() string{
+		fmt.Printf("hello world");
+		return "hello world"
+	
+}
 func add(a int , b int) int {
 		return a + b
 }
 func calc(a int ,b int) (int , int ) {
 		return a + b ,a - b
+}
+
+func sayHelloWorld() string {
+	return "hello world"
 }
 //标识符
 //关键字
@@ -33,7 +43,6 @@ func  var_test1() {
 		var c bool
 		var d int = 8
 		var e string = "hello world"
-
 		fmt.Println(a,b,c,d,e)
 	
 }
@@ -42,31 +51,25 @@ func  var_test1() {
 func Test1(){
 		fmt.Println("this is Test1");
 }
-
 func  con_test1(){
 		const a int = 9;
 		const b string = "hello world";
 		const c bool = false;
 		const d = 9;
 		fmt.Println(a,b,c,d);
-
 		const (
 				e string = "hello c"
 				f int = 0;
 				g 
 		)
 		fmt.Println(e,f,g);
-
 		const (
 				h =  1<<iota
 				j
 				k
-
 		)
 
 		fmt.Println(h,j,k);
-
-
 }
 
 
